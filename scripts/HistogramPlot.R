b9889736244963656b3a3aab40d877b8ee87a986
Zach Palmer <zachbhs@uw.edu>
2017-05-30 22:32:29 -0800

    Touched up graph

--- scripts/HistogramPlot.R
+++ scripts/HistogramPlot.R
@@ -4,10 +4,11 @@ library(plotly)
 HistogramPlot <- function(data, type.plot) {
   return (switch (type.plot,
                   personal = plot_ly(data, alpha = 0.6) %>%
-                    add_histogram(x = ~p_income) %>%
-                    add_histogram(x = ~county_income) %>%
-                    layout(barmode = "overlay"),
-                  compared = plot_ly(data, x = ~comp_income, type = "histogram"),
-                  unemployment = plot_ly(data, x = ~urate, type = "histogram"),
-                  college = plot_ly(data, x = ~college, type = "histogram")))
+                    add_histogram(x = ~p_income, name = 'Personal Income') %>%
+                    add_histogram(x = ~county_income, name = 'County Average Income') %>%
+                    layout(barmode = "overlay", title = "Personal Income vs County Average Income", yaxis = list(title = 'Count'), xaxis = list(title = 'Income ($)')),
+                  compared = plot_ly(data, x = ~comp_income, type = "histogram") %>% layout(title = "Average Comparative Income", yaxis = list(title = 'Count'), xaxis = list(title = 'Comparative Income Score')),
+                  unemployment = plot_ly(data, x = ~urate, type = "histogram") %>% layout(title = "Average County Unemployment", yaxis = list(title = 'Count'), xaxis = list(title = '% of People Unemployed')),
+                  college = plot_ly(data, x = ~college, type = "histogram") %>% layout(title = "Number of People with College Degrees", yaxis = list(title = 'Count'), xaxis = list(title = '% of People with College Degrees'))))
 }
+
