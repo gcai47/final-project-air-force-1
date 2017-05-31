3777d7e966dd8274847a920b1dd9b8e6f586efee
Richard Jiang <Richard Jiang>
2017-05-30 21:40:27 -0700

    Started ScatterPlot

--- scripts/HistogramPlot.R
+++ scripts/HistogramPlot.R
@@ -13,9 +13,9 @@ killing.data <- na.omit(killing.data)
 killing.data[] <- lapply(killing.data, function(x) as.numeric(as.character(x)))
 
 personal.income <- plot_ly(killing.data, alpha = 0.6) %>%
-add_histogram(x = ~p_income) %>%
-add_histogram(x = ~county_income) %>%
-layout(barmode = "overlay")
+  add_histogram(x = ~p_income) %>%
+  add_histogram(x = ~county_income) %>%
+  layout(barmode = "overlay")
 
 
 compincome <- plot_ly(killing.data, x = ~comp_income, type = "histogram")
@@ -24,5 +24,6 @@ unemployment <- plot_ly(killing.data, x = ~urate, type = "histogram")
 
 
 college <- plot_ly(killing.data, x = ~college, type = "histogram")
+
 
 
