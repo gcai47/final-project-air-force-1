f727e4f7923a00ae1930f2a1e640f0269c1986d2
Richard Jiang <Richard Jiang>
2017-05-31 17:39:01 -0700

    Finished ScatterPlot

--- scripts/ScatterPlot.R
+++ scripts/ScatterPlot.R
@@ -1,11 +1,88 @@
 library(plotly)
 
 ScatterPlot <- function(scatter.data, input) {
-  ggp <- ggplot(scatter.data, aes(x = eval(parse(text = input$ScatterX)), y = eval(parse(text = input$ScatterY)), colour = eval(parse(text = input$ScatterX)))) +
+  if (input$ScatterX == 'age') {
+    x <- "Age"
+  } else if (input$ScatterX == 'gender') {
+    x <- "Gender"
+  } else if (input$ScatterX == 'raceethnicity') {
+    x <- "Ethnicity"
+  } else if (input$ScatterX == 'cause') {
+    x <- "Cause of Death"
+  } else if (input$ScatterX == 'armed') {
+    x <- "Victim's Arms"
+  } else if (input$ScatterX == 'pop') {
+    x <- "Population of County"
+  } else if (input$ScatterX == 'share_white') {
+    x <- "Percentage of White Population"
+  } else if (input$ScatterX == 'share_black') {
+    x <- "Percentage of Black Population"
+  } else if (input$ScatterX == 'share_hispanic') {
+    x <- "Percentage of Hispanic Population"
+  } else if (input$ScatterX == 'p_income') {
+    x <- "Personal Income"
+  } else if (input$ScatterX == 'h_income') {
+    x <- "Household Income"
+  } else if (input$ScatterX == 'county_income') {
+    x <- "% Average Income"
+  } else if (input$ScatterX == 'comp_income') {
+    x <- "Income Relative to Average" 
+  } else if (input$ScatterX == 'county_bucket') {
+    x <- "Quintile of County Income"
+  } else if (input$ScatterX == 'nat_bucket') {
+    x <- "Quintile of National Income"
+  } else if (input$ScatterX == 'pov') {
+    x <- "County Poverty Rate"
+  } else if (input$ScatterX == 'urate') {
+    x <- "County Unemployment Rate"
+  } else if (input$ScatterX == 'college') {
+    x <- "College Education Rate"
+  }
+  
+  if (input$ScatterY == 'age') {
+    y <- "Age"
+  } else if (input$ScatterY == 'gender') {
+    y <- "Gender"
+  } else if (input$ScatterY == 'raceethnicity') {
+    y <- "Ethnicity"
+  } else if (input$ScatterY == 'cause') {
+    y <- "Cause of Death"
+  } else if (input$ScatterY == 'armed') {
+    y <- "Victim's Arms"
+  } else if (input$ScatterY == 'pop') {
+    y <- "Population of County"
+  } else if (input$ScatterY == 'share_white') {
+    y <- "Percentage of White Population"
+  } else if (input$ScatterY == 'share_black') {
+    y <- "Percentage of Black Population"
+  } else if (input$ScatterY == 'share_hispanic') {
+    y <- "Percentage of Hispanic Population"
+  } else if (input$ScatterY == 'p_income') {
+    y <- "Personal Income"
+  } else if (input$ScatterY == 'h_income') {
+    y <- "Household Income"
+  } else if (input$ScatterY == 'county_income') {
+    y <- "% Average Income"
+  } else if (input$ScatterY == 'comp_income') {
+    y <- "Income Relative to Average" 
+  } else if (input$ScatterY == 'county_bucket') {
+    y <- "Quintile of County Income"
+  } else if (input$ScatterY == 'nat_bucket') {
+    y <- "Quintile of National Income"
+  } else if (input$ScatterY == 'pov') {
+    y <- "County Poverty Rate"
+  } else if (input$ScatterY == 'urate') {
+    y <- "County Unemployment Rate"
+  } else if (input$ScatterY == 'college') {
+    y <- "College Education Rate"
+  }
+  
+  ggp <- ggplot(scatter.data, aes(x = eval(parse(text = input$ScatterX)), y = eval(parse(text = input$ScatterY)), colour = eval(parse(text = input$ScatterX)), fill = eval(parse(text = input$ScatterY)))) +
     geom_point(position = 'jitter', na.rm = TRUE) +
-    labs(title = paste0(input$ScatterX, ' vs. ', input$ScatterY), x = input$ScatterX, y = input$ScatterY)
+    labs(title = paste0(x, ' vs. ', y, ' of Shooting Victims'), x = x, y = y, colour = x) +
+    theme(legend.position = 'none')
   
   # Converts to plotly graph and adds hovertext
-  plot <- ggplotly(ggp)
+  plot <- ggplotly(ggp, tooltip = NULL)
   return(plot)
 }
--- ui.R
+++ ui.R
@@ -42,17 +42,17 @@ shinyUI(fluidPage(
                
       ),
       
-      tabPanel("Scatter Plot", 
-               titlePanel("Scatter Plot"),
+      tabPanel("Attributes", 
+               titlePanel("Comparing Attributes of Shooting Victims"),
                mainPanel(
                  plotlyOutput('Scatter')
                ),
                sidebarPanel(
-                 selectInput("ScatterX", label = h3("X-Axis"), 
-                             choices = list("Age" = 'age', "Gender" = 'gender', "Ethnicity" = 'raceethnicity', "Cause of Death" = 'cause', "Victim's Arms" = 'armed', "Population of County" = 'pop', "Percentage of White Population" = 'share_white', "Percentage of Black Population" = 'share_black', "Percentage of Hispanic Population" = 'share_hispanic', "Personal Income" = 'p_income', "Household Income" = 'h_income', "Average County Income" = 'county_income', "Income Relative to Average" = 'comp_income', "Quintile of County Income" = 'county_bucket', "Quintile of National Income" = 'nat_bucket', "County Poverty Rate" = 'pov', "County Unemployment Rate" = 'urate', "College Education Rate" = 'college'), 
+                 selectInput("ScatterX", label = h3("Select X-Axis Variable"), 
+                             choices = list("Age" = 'age', "Gender" = 'gender', "Ethnicity" = 'raceethnicity', "Cause of Death" = 'cause', "Victim's Arms" = 'armed', "Population of County" = 'pop', "Percentage of White Population" = 'share_white', "Percentage of Black Population" = 'share_black', "Percentage of Hispanic Population" = 'share_hispanic', "Personal Income" = 'p_income', "Household Income" = 'h_income', "Average County Income" = 'county_income', "% Average Income" = 'comp_income', "Quintile of County Income" = 'county_bucket', "Quintile of National Income" = 'nat_bucket', "County Poverty Rate" = 'pov', "County Unemployment Rate" = 'urate', "College Education Rate" = 'college'), 
                              selected = 'age'),
-                 selectInput("ScatterY", label = h3("Y-Axis"), 
-                             choices = list("Age" = 'age', "Gender" = 'gender', "Ethnicity" = 'raceethnicity', "Cause of Death" = 'cause', "Victim's Arms" = 'armed', "Population of County" = 'pop', "Percentage of White Population" = 'share_white', "Percentage of Black Population" = 'share_black', "Percentage of Hispanic Population" = 'share_hispanic', "Personal Income" = 'p_income', "Household Income" = 'h_income', "Average County Income" = 'county_income', "Income Relative to Average" = 'comp_income', "Quintile of County Income" = 'county_bucket', "Quintile of National Income" = 'nat_bucket', "County Poverty Rate" = 'pov', "County Unemployment Rate" = 'urate', "College Education Rate" = 'college'), 
+                 selectInput("ScatterY", label = h3("Select Y-Axis Variable"), 
+                             choices = list("Age" = 'age', "Gender" = 'gender', "Ethnicity" = 'raceethnicity', "Cause of Death" = 'cause', "Victim's Arms" = 'armed', "Population of County" = 'pop', "Percentage of White Population" = 'share_white', "Percentage of Black Population" = 'share_black', "Percentage of Hispanic Population" = 'share_hispanic', "Personal Income" = 'p_income', "Household Income" = 'h_income', "Average County Income" = 'county_income', "% Average Income" = 'comp_income', "Quintile of County Income" = 'county_bucket', "Quintile of National Income" = 'nat_bucket', "County Poverty Rate" = 'pov', "County Unemployment Rate" = 'urate', "College Education Rate" = 'college'), 
                              selected = 'gender')
                )
                
